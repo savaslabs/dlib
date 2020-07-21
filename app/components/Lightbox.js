a16048dd3c103cd3315385f86164e96d1af089bc
Madeline Streilein <mcstreilein@gmail.com>
2020-07-21 19:11:08 -0400

    Convert lightbox to functional component

--- app/components/Lightbox.js
+++ app/components/Lightbox.js
@@ -1,40 +1,36 @@
-import React, { Component } from 'React';
+import React from 'react';
 import Lightbox from 'react-accessible-lightbox';
 
-export default class lightbox extends Component {
-  render() {
-    const {
-      imageIds,
-      imageCaptions,
-      photoIndex,
-      isOpen,
-      closeLightbox,
-      nextLightboxImage,
-      eventPage
-    } = this.props;
-
-    let location;
-    if (eventPage) {
-      location = "../app";
-    } else {
-      location = 'app';
-    }
-    return (
-      <>
-        {isOpen && (
-          <Lightbox
-            mainSrc={`${location}/assets/images/${imageIds[photoIndex]}/full.jpg`}
-            nextSrc={`${location}/assets/images/${
-              imageIds[(photoIndex + 1 < imageIds.length)
-                ? photoIndex + 1
-                : 0]
-            }/full.jpg`}
-            onCloseRequest={() => closeLightbox()}
-            onMoveNextRequest={() => nextLightboxImage()}
-            imageCaption={imageCaptions[photoIndex]}
-          />
-        )}
-      </>
-    );
+const lightbox = ({
+  imageIds,
+  imageCaptions,
+  photoIndex,
+  isOpen,
+  closeLightbox,
+  nextLightboxImage,
+  eventPage,
+}) => {
+  let location;
+  if (eventPage) {
+    location = '../app';
+  } else {
+    location = 'app';
   }
-}
+  return (
+    <>
+      {isOpen && (
+        <Lightbox
+          mainSrc={`${location}/assets/images/${imageIds[photoIndex]}/full.jpg`}
+          nextSrc={`${location}/assets/images/${
+            imageIds[photoIndex + 1 < imageIds.length ? photoIndex + 1 : 0]
+          }/full.jpg`}
+          onCloseRequest={() => closeLightbox()}
+          onMoveNextRequest={() => nextLightboxImage()}
+          imageCaption={imageCaptions[photoIndex]}
+        />
+      )}
+    </>
+  );
+};
+
+export default lightbox;
