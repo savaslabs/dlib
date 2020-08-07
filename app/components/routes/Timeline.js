7d93a265e303877ea310aebd27b7183c748942df
Madeline Streilein <mcstreilein@gmail.com>
2020-08-07 10:31:40 -0400

    Re-add useRef to timeline

--- app/components/routes/Timeline.js
+++ app/components/routes/Timeline.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { cleanId, cleanJSON } from '../../utils/constants';
 import { Link } from 'react-router-dom';
 import TimelineKey from '../TimelineKey';
