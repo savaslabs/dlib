34baa6c13d750b1134fc37ff24468a3c9673bd5b
Madeline Streilein <mcstreilein@gmail.com>
2020-08-12 15:19:03 -0400

    Prerender html with react-snap

--- app/index.js
+++ app/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import { render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import App from './App';
 
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('app')
-);
+const rootElement = document.getElementById('app');
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
+} else {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
+}
