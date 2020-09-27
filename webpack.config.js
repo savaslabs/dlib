ae0d9e56537233eb43cf798313b7036f58ea0b0a
Madeline Streilein <mcstreilein@gmail.com>
2020-09-27 11:33:11 -0400

    Update webpack config for caching

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env, arg) => {
   const isProduction = arg.mode === 'production' ? true : false;
@@ -99,6 +99,7 @@ module.exports = (env, arg) => {
       new HtmlWebpackPlugin({
         template: 'app/index.html',
       }),
+      new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
         filename: 'index.css',
         path: path.resolve(__dirname, 'dist'),
